ab0acdb970fe4b3e6b5a2f519d18d58bd4517ce5
mrongSubho <mrongsubho@gmail.com>
2026-02-23 09:54:13 +0600

    fix: merged webpack and build ignore settings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
+  },
+
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
